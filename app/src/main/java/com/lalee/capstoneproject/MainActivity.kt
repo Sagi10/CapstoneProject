44ea35505b285d5141c1b1fc09861897a25ad438
Sagi <sagi.lalee@hva.nl>
2020-12-07 22:08:40 +0100

    Getting pictures from URI

--- app/src/main/java/com/lalee/capstoneproject/MainActivity.kt
+++ app/src/main/java/com/lalee/capstoneproject/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.lalee.capstoneproject
 
+import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.navigation.findNavController
 import com.lalee.capstoneproject.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_camera.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +41,8 @@ class MainActivity : AppCompatActivity() {
             findNavController(R.id.nav_host_fragment).navigate(R.id.action_HomeFragment_to_CameraFragment)
         }
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/lalee/capstoneproject/ui/CameraFragment.kt
+++ app/src/main/java/com/lalee/capstoneproject/ui/CameraFragment.kt
@@ -1,8 +1,11 @@
 package com.lalee.capstoneproject.ui
 
+import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -13,11 +16,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.lalee.capstoneproject.R
 import kotlinx.android.synthetic.main.fragment_camera.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 private const val REQUEST_IMAGE_CAPTURE = 1
+lateinit var currentPhotoPath: String
 
 class CameraFragment : Fragment() {
 
@@ -26,8 +35,8 @@ class CameraFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false)
@@ -36,26 +45,78 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        openDefaultCamera()
+        //openDefaultCamera()
+        dispatchTakePictureIntent()
     }
 
-    private fun openDefaultCamera() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun dispatchTakePictureIntent() {
+        val pictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        if (pictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+            var photoFile: File? = null
 
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(activity, "ERROR: ${e.message}", Toast.LENGTH_SHORT).show()
+            try {
+                photoFile = createImageFile()
+            } catch (ex: IOException) {
+                //error
+            }
+
+            if (photoFile != null) {
+                val photoURI = FileProvider.getUriForFile(
+                    requireContext(),
+                    "com.lalee.capstoneproject.fileprovider",
+                    photoFile
+                )
+                pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
+
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ) // directory)
+        currentPhotoPath = image.absolutePath
+
+        return image
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            //set the taken picture to imageView
-            imageView.setImageBitmap(imageBitmap)
+
+            val file = File(currentPhotoPath)
+            Log.i(TAG, "FILE: ${file.absolutePath}")
+            Log.i(TAG, "DATA: ${data?.data}")
+
+
+            imageView.setImageURI(Uri.fromFile(file))
+
+
+            //val imageBitmap = data?.extras?.get("data") as Bitmap
+            //imageView.setImageBitmap(imageBitmap)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
+
+//    private fun openDefaultCamera() {
+//        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//
+//        try {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//
+//        } catch (e: ActivityNotFoundException) {
+//            Toast.makeText(activity, "ERROR: ${e.message}", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
 }
